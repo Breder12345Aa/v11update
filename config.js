8b1798424e8976c9c0da80683cfafcb6d4dfdea5
Breder12345Aa <82520506+Breder12345Aa@users.noreply.github.com>
2022-07-04 11:09:51 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,9 +2,9 @@ let fs = require('fs')
 let chalk = require('chalk')
 
 global.owner = [
-  ['6287898307350'],
-  ['6287898307350'],
-  ['6287898307350', 'Owner', true]
+  ['6283183321692'],
+  ['6283183321692'],
+  ['6283183321692', 'Owner', true]
   // [number, dia creator/owner?, dia developer?]
 ] // Put your number here
 global.mods = [] // Want some help?
